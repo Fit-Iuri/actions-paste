0d3436ea0628c63d65022fb536b2bdce080be260
Fit-Iuri <iuri.mendes@fitbank.com.br>
2022-07-20 17:55:30 +0000

    Update from https://github.com/Fit-Iuri/github-actions-training/commit/26cf920f2b94ad2c53730b0c91f6c46c4219835b

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export const Home: React.FC = () => {
+  return (
+    <div>
+      <div>Olá!!!</div>
+      <div>Testando GitHub Actions...</div>
+      <div>AAAAAAAAAAAAAAAAAAAAAAAAAAAAA</div>
+    </div>
+  );
+};
